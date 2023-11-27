d86ec282ece0de154717984a2fc1a5c83e986b06
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2023-11-27 17:27:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,11 @@ try {
 
 // Load settings into memory
 const settings = require("./settings.json");
-
 // Setup basic express server
 var express = require('express');
 var app = express();
 if (settings.express.serveStatic)
-	app.use(express.static('../build/www'));
+	app.use(express.static('./build/www'));
 var server = require('http').createServer(app);
 
 // Init socket.io
@@ -60,20 +59,17 @@ const log2 = Log2.log;
 // Load ban list
 const Ban = require('./ban.js');
 Ban.init();
-var wtf = 
-['I hate pope beggars.', 'Time to meme!', 'A C I D  M O D E', "Do not beg!", 'Please, give me mercy!', 'No flooding!', 'UH OH RETARD ALERT', '** ERROR INTERNAL SYSTEM FAILURE **', 'Fagola!', 'Turret fagolas!', 'FUCK!', 'NO!\n MY MOM HAS A FIREWALL', "Eliot's Fun Box", "FUCK EM UP"]
+
 // Start actually listening
 server.listen(port, function () {
-	console.log(
-		" Welcome to BonziWORLD: Revived!\n",
-		" " + wtf[Math.floor(Math.random()*wtf.length)] + "\n", 
-		"----------------------\n",
-		"Server listening at port " + port,
-	);
+  console.log(
+    " Welcome to BonziWORLD!\n",
+    "Time to meme!\n",
+    "----------------------\n",
+    "Server listening at port " + port
+  );
 });
 app.use(express.static(__dirname + '/public'));
-
-
 
 // ========================================================================
 // Banning functions
