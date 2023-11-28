2920c1bc2c238fb3e99bf60ed4c1bca55ae70ae6
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2023-11-27 19:33:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ var express = require('express');
 var app = express();
 if (settings.express.serveStatic)
 	app.use(express.static('.//build/www'));
-app.listen(process.env.PORT);
 var server = require('http').createServer(app);
 
 // Init socket.io
