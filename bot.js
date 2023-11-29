4a7735836c3bb11165f82d15a49c33f65b610544
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2023-11-28 21:22:47 -0600

    Update bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require("fs");
+
+
+client.login("NTYyNjczNDc1NjQ3NTY5OTcw.XXptYA.M5W0lvufODUhv-pAWvJrb63vc5o") 
